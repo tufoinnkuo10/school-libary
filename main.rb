3d134f1a37d8462699e5d99fa4713df04b6b3224
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-12-23 17:11:16 +0100

    import other classes and add file part

---
+++ main.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require_relative './person'
+require_relative './student'
+require_relative './rental'
+require_relative './book'
+require_relative './teacher'
+require_relative './clasroom'
+
+# Initialize the App class
