1068e35bfe593981de1a4cd2205658bd0e8eb7de
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-12-23 17:49:23 +0100

    add break options

--- main.rb
+++ main.rb
@@ -15,4 +15,43 @@ class App
     @people = []
   end
 
+  def run
+    puts 'Welcome to our School Library App!'
+
+    loop do
+      actions
+
+      option = gets.chomp
+
+      break if option >= 7
+
+      handle_action option
+    end
+
+    puts 'Hope the app was great!'
+  end
+
+  private
+
+  def handle_action(option)
+    case option
+    when '1'
+      list_books
+    when '2'
+      list_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals
+    else
+      puts 'This option is invalid/not available'
+    end
+  end
+
   
+
+main
